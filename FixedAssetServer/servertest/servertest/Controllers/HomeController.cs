8ed4fb3a2ba283e627e5f208fe406bc5fd9d1c2c
MrZhengxhao <113524184+MrZhengxhao@users.noreply.github.com>
2022-09-14 21:19:59 +0800

    Add files via upload

---
+++ FixedAssetServer/servertest/servertest/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using JJY.NC.Core.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FixedAssetsService.Controllers
+{
+    public class HomeController : BaseController
+    {
+
+    }
+}
---
+++ FixedAssetServer/servertest/servertest/Controllers/Test.cs
@@ -0,0 +1,58 @@
+﻿using JJY.NC.Core.Controllers;
+using JJYRest;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace FixedAssetsService.Controllers
+{
+    /// <summary>
+    /// 【0】 测试  http://127.0.0.1:9999/swagger/index.html
+    /// </summary>
+    public class Test : BaseController
+    {
+        [HttpPost(nameof(Add))]
+        public object Add(Request<ArgsData> args)
+        {
+            var data = args.Arg1;
+
+            data.P1 = "Success";
+
+            return data.NewResultJsonStr();
+        }
+
+        /// <summary>
+        /// http://127.0.0.1:9999/demo/test/add
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet(nameof(Add))]
+        public object Add_Get()
+        {
+            var rs = DateTime.Now.ToFullString();
+
+            return rs;
+        }
+    }
+
+    public class ArgsData
+    {
+
+        public string P1 { get; set; }
+
+
+        public int P2 { get; set; }
+
+        public DateTime P3 { get; set; }
+
+        public List<ArgsDataItem> Items { get; set; }
+    }
+
+    public class ArgsDataItem
+    {
+
+        public string PI1 { get; set; }
+
+
+    }
+
+}
